36506b235e098a7a715dceb6c2ac8e97cb9d5494
Kevin TSI <kevtsiii@gmail.com>
2021-02-03 11:30:12 +0100

    Push 03/02/2021 matinée

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import subprocess
+from models import Payments
 
 config = {
     "host" : "localhost",
     "port" : 3306,
-    "user" : "root",
-    "password" : "root",
+    "user" : "kevin",
+    "password" : "motdepasse",
     "database" : "classicmodels"
 }
 
@@ -19,7 +21,7 @@ connection_str = "mysql+pymysql://{}:{}@{}:{}/{}".format(db_user,db_pwd,db_host,
 print(connection_str)
 try:
     engine = create_engine(connection_str)
-    engine.connect()
+    conn = engine.connect()
     print("---MySQL Docker Container Python connection ok---\n")
 except:
     print("Something went wrong\n")
@@ -31,11 +33,25 @@ subprocess_return = subprocess.stdout.read()
 
 print("--- docker ps info ---\n")
 print(subprocess_return,"\n")
+
 print("--- Data Mapping OK ---\n")
 for name in engine.table_names():
     print("- {}\n".format(name))
 
+print("---  Selection of PAYMENTS table ---\n")
+print("Type : {} \n".format(type(Payments.columns.customerNumber)))
 
+print("Columns : {}\n\n".format(engine.table_names()))
+
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+print("### All payments with > 01 June 2005 : \n")
+result = session.query(Payments).filter(Payments.c.paymentDate > "2005-06-01").all()
+
+for row in result:
+    print("CustomerID : {} , CheckNumber : {}, PaymentDate : {}, Amount : {}".format(row.customerNumber, row.checkNumber, row.paymentDate, row.amount))
 
 
 
--- models.py
+++ models.py
@@ -1,1 +1,12 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, Numeric
+from sqlalchemy import MetaData, Table
+
+metadata = MetaData()
+
+Payments = Table("payments", 
+metadata, 
+Column("customerNumber", Integer, primary_key=True, nullable=False),
+Column("checkNumber", Integer, primary_key=True, nullable=False),
+Column("paymentDate",Date, nullable=False),
+Column("amount",Numeric, nullable=False)
+)
